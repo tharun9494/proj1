5d9bc7ff48b95ad0809bfd7d8b100e9b06aece2b
Ontimitta tharun <ontimittatharun@Ontimittas-MacBook-Air.local>
2025-03-27 23:39:06 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Change '/' to './' to support relative paths
+  base: './',  // Ensure this is set correctly
+  build: {
+    outDir: 'dist',  // Ensures Vercel picks the correct folder
+  },
   server: {
     port: 3000,
   },
-  build: {
-    outDir: 'dist',
-  }
 });
