3c8a48f3ca0747694a7545a6373c7eb663101a44
Ontimitta tharun <ontimittatharun@Ontimittas-MacBook-Air.local>
2025-03-27 23:06:09 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
+    historyApiFallback: true
   },
+  build: {
+    outDir: 'dist'
+  }
 });
