1c98a55d5c52d32997f954f8a97784607f831263
Ontimitta tharun <ontimittatharun@Ontimittas-MacBook-Air.local>
2025-03-27 21:38:08 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures proper routing for Vite
-  build: {
-    outDir: "dist",
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
   server: {
-    historyApiFallback: true, // Handles SPA routing in development
-  }
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
 });
