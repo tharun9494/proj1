799fa3ea8d457dff91b6dd9c855783b73435ee0c
Ontimitta tharun <ontimittatharun@Ontimittas-MacBook-Air.local>
2025-03-27 23:31:07 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './', // Change '/' to './' to support relative paths
   server: {
     port: 3000,
   },
